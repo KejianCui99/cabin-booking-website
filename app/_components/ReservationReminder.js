a885787bad43bccf2222d039f17bcd5d9e10aee0
Kejian Cui <kejiancui99@gmail.com>
2024-09-25 11:58:56 +1000

    resolve typo in ReservationReminder.js

--- app/_components/ReservationReminder.js
+++ app/_components/ReservationReminder.js
@@ -12,7 +12,7 @@ function ReservationReminder() {
   return (
     <div className="fixed bottom-6 left-1/2 -translate-x-1/2 py-5 px-8 rounded-full bg-accent-500 text-primary-800 text  font-semibold shadow-xl shadow-slate-900 flex gap-8 items-center">
       <p>
-        <span>👋</span> Don'f forget to reserve your dates <br /> from{" "}
+        <span>👋</span> Don&apos;t forget to reserve your dates <br /> from{" "}
         {format(new Date(range.from), "MMM dd yyyy")} to{" "}
         {format(new Date(range.to), "MMM dd yyyy")}
       </p>
