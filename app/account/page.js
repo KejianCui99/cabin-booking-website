33b5f2a93b8a7396d7a096faebb3eb3a1a997b6c
Kejian Cui <kejiancui99@gmail.com>
2024-09-28 15:28:53 +1000

    updated css for profile

--- app/account/page.js
+++ app/account/page.js
@@ -9,7 +9,7 @@ export default async function Page() {
   const firstName = session.user.name.split(" ").at(0);
 
   return (
-    <h2 className="font-semibold text-2xl text-accent-400">
+    <h2 className="font-semibold text-2xl text-accent-400 mb-7">
       Welcome, {firstName}
     </h2>
   );
