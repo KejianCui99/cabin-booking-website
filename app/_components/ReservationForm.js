cfc18fc55e51f87fe980140df757ca46b714735b
Kejian Cui <kejiancui99@gmail.com>
2024-09-30 16:39:33 +1000

    update create new reservation function

--- app/_components/ReservationForm.js
+++ app/_components/ReservationForm.js
@@ -1,11 +1,27 @@
 "use client";
 
+import { differenceInDays } from "date-fns";
 import { useReservation } from "./ReservationContext";
+import { createBooking } from "../_lib/actions";
 
 function ReservationForm({ cabin, user }) {
-  const { range } = useReservation();
-  // CHANGE
-  const { maxCapacity } = cabin;
+  const { range, resetRange } = useReservation();
+
+  const { maxCapacity, regularPrice, discount } = cabin;
+  const startDate = range.from;
+  const endDate = range.to;
+  const numNights = differenceInDays(endDate, startDate);
+  const cabinPrice = numNights * (regularPrice - discount);
+
+  const bookingData = {
+    startDate,
+    endDate,
+    numNights,
+    cabinPrice,
+    cabinId: id,
+  };
+
+  const createBookingWithData = createBooking.bind(null, bookingData); // createBooking with its first parameter bound to bookingData
 
   return (
     <div className="scale-[1.01]">
@@ -26,7 +42,13 @@ function ReservationForm({ cabin, user }) {
         }
       </div>
 
-      <form className="bg-primary-900 py-10 px-16 text-lg flex gap-5 flex-col">
+      <form
+        action={async (formData) => {
+          await createBookingWithData(formData);
+          resetRange();
+        }}
+        className="bg-primary-900 py-10 px-16 text-lg flex gap-5 flex-col"
+      >
         <div className="space-y-2">
           <label htmlFor="numGuests">How many guests?</label>
           <select
